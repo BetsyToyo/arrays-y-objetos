d004926d0579a2a40352296c7f18925a7fbcc9ef
Betsy Toyo <btoyo6009@e-camp.cl>
2022-01-22 13:59:36 -0300

    comentarios

--- uso-variables-operadores-aritmeticos/assets/js/app.js
+++ uso-variables-operadores-aritmeticos/assets/js/app.js
@@ -1,15 +1,17 @@
 //Ejercicio 1
 
-var numero1= prompt("ingrese primer numero para operaciones aritmeticas",""); 
+var numero1= prompt("ingrese primer numero para operaciones aritmeticas",""); //se le piden al usuario el primer numero
 
-var numero2= prompt("ingrese segundo numero para operaciones aritmeticas","");
 
-var suma= parseFloat(numero1) + parseFloat(numero2);
-var resta= parseFloat(numero1) - parseFloat(numero2);
-var multiplicacion= parseFloat(numero1) * parseFloat(numero2);
-var division= parseFloat(numero1) / parseFloat(numero2);
-var modulo= parseFloat(numero1) % parseFloat(numero2);
+var numero2= prompt("ingrese segundo numero para operaciones aritmeticas",""); //se le piden al usuario el segundo numero
 
+var suma= parseFloat(numero1) + parseFloat(numero2); //se realiza la suma
+var resta= parseFloat(numero1) - parseFloat(numero2); //se realiza la resta
+var multiplicacion= parseFloat(numero1) * parseFloat(numero2); //se realiza la multiplicacion
+var division= parseFloat(numero1) / parseFloat(numero2); //se realiza la division
+var modulo= parseFloat(numero1) % parseFloat(numero2); //se realiza el modulo
+
+//se envian a pantalla los resultados
 document.getElementById('tabla').innerHTML += `
     <tr>
         <td>${numero1}</td>
@@ -24,14 +26,14 @@ document.getElementById('tabla').innerHTML += `
 
 
 
-
 //Ejercicio 2
-var temperaturaCelsius= prompt("ingrese la temperatura en Celsius para tranformar","");
+var temperaturaCelsius= prompt("ingrese la temperatura en Celsius para tranformar","");//se le piden al usuario el primer numero
 
-var temperaturaFahrenheit= parseFloat(temperaturaCelsius) * parseFloat(9/5) + 32 
+var temperaturaFahrenheit= parseFloat(temperaturaCelsius) * parseFloat(9/5) + 32 //se transforma la temperatura de Celsius a Fahrenheit
 
-var temperaturaKelvin= parseFloat(temperaturaCelsius) + parseFloat(273.15) 
+var temperaturaKelvin= parseFloat(temperaturaCelsius) + parseFloat(273.15) //se transforma la temperatura de Celsius a Kelvin
 
+//se envian a pantalla los resultados
 document.getElementById('tabla-temperaturas').innerHTML +=`
 <tr>
     <td>${temperaturaCelsius}</td>
@@ -44,13 +46,13 @@ document.getElementById('tabla-temperaturas').innerHTML +=`
 
 //Ejercicio 3
 
-var dias= prompt("ingrese el numero de dias para transformar a semanas y años","");
+var dias= prompt("ingrese el numero de dias para transformar a semanas y años",""); //se le piden al usuario el numero de dias
 
-var años = Math.floor(dias/365);
-var semanas = Math.floor((dias%365)/7)
-var diasFinal = Math.floor(((dias%365)%7))
+var años = Math.floor(dias/365); //se transforma los dias a años
+var semanas = Math.floor((dias%365)/7)  //se transforma los dias a semanas
+var diasFinal = Math.floor(((dias%365)%7)) //se transforma los dias a dias
 
-
+//se envian a pantalla los resultados
 document.getElementById('tabla-tiempo').innerHTML +=`
 <tr>
     <td>${años}</td>
@@ -63,17 +65,19 @@ document.getElementById('tabla-tiempo').innerHTML +=`
 
 //Ejercicio 4
 
-var primerNumero= prompt("ingrese el primer numero para suma y promedio","");
-var segundoNumero= prompt("ingrese el segundo numero para suma y promedio","");
-var tercerNumero= prompt("ingrese el tercer numero para suma y promedio","");
-var cuartoNumero= prompt("ingrese el cuarto numero para suma y promedio","");
-var quintoNumero= prompt("ingrese el quinto numero para suma y promedio","");
+var primerNumero= prompt("ingrese el primer numero para suma y promedio",""); //se le piden al usuario el primer numero
+var segundoNumero= prompt("ingrese el segundo numero para suma y promedio",""); //se le piden al usuario el primer numero
+var tercerNumero= prompt("ingrese el tercer numero para suma y promedio",""); //se le piden al usuario el primer numero
+var cuartoNumero= prompt("ingrese el cuarto numero para suma y promedio",""); //se le piden al usuario el primer numero
+var quintoNumero= prompt("ingrese el quinto numero para suma y promedio",""); //se le piden al usuario el primer numero
 
+    //se realiza la suma
 var sumaNumeros= parseFloat(primerNumero)+parseFloat(segundoNumero)+parseFloat(tercerNumero)+parseFloat(cuartoNumero)+parseFloat(quintoNumero);
 
+    //se calcula el promedio
 var promedioNumeros= (parseFloat(primerNumero)+parseFloat(segundoNumero)+parseFloat(tercerNumero)+parseFloat(cuartoNumero)+parseFloat(quintoNumero))/5
 
-
+//se envian a pantalla los resultados
 document.getElementById('tabla-promedio').innerHTML +=`
 <tr>
     <td>${primerNumero}, ${segundoNumero}, ${tercerNumero}, ${cuartoNumero}, ${quintoNumero}</td>
